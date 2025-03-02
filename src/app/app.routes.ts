780064cb62787d9ea23260177478aaf6387f658d
Wilmer Betancourt <wilmergabybet@gmail.com>
2025-03-02 10:59:34 -0500

    Cambios pagina estilo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { Component } from '@angular/core';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { ProductosComponent } from './pages/productos/productos.component';
+import { ContactosComponent } from './pages/contactos/contactos.component';
+import { Error404Component } from './pages/error404/error404.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'home', component: HomeComponent},
+    {path:'nosotros', component: NosotrosComponent},
+    {path:'productos', component: ProductosComponent},
+    {path: 'contactos', component: ContactosComponent},
+
+    {path: '', redirectTo:'home', pathMatch:'full'},
+
+    {path:'**', component:Error404Component},
+
+
+
+
+];
